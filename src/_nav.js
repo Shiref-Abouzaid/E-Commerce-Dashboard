43a1bc7a94b138864e32cf8baadf2411adc80ce4
sherif <sherif@mv-is.com>
2022-05-27 15:38:31 +0200

    before start

---
+++ src/_nav.js
@@ -0,0 +1,70 @@
+export default [
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Dashboard',
+  //   to: '/dashboard',
+  //   icon: 'cil-speedometer',
+  //   badge: {
+  //     color: 'primary',
+  //     text: 'NEW',
+  //   },
+  // },
+
+  {
+    component: 'CNavItem',
+    name: 'Create User',
+    to: '/users/create',
+    icon: 'cis-people-plus',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Posts',
+    to: '/theme/typography',
+    icon: 'cil-pencil',
+  },
+  {
+    component: 'CNavGroup',
+    name: 'Pages',
+    to: '/pages',
+    icon: 'cil-star',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'Login',
+        to: '/pages/login',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Register',
+        to: '/pages/register',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Error 404',
+        to: '/pages/404',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Error 500',
+        to: '/pages/500',
+      },
+    ],
+  },
+
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Download CoreUI',
+  //   href: 'http://coreui.io/vue/',
+  //   icon: { name: 'cil-cloud-download', class: 'text-white' },
+  //   _class: 'bg-success text-white',
+  //   target: '_blank'
+  // },
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Try CoreUI PRO',
+  //   href: 'http://coreui.io/pro/vue/',
+  //   icon: { name: 'cil-layers', class: 'text-white' },
+  //   _class: 'bg-danger text-white',
+  //   target: '_blank'
+  // }
+]
---
+++ src/views/pages/users/create.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>create user</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateUser',
+}
+</script>
